44889d74d85896dd3c4793107dbf54f77e2c19f7
creallfluharty <creallfluharty@gmail.com>
2023-03-05 11:06:06 -0600

    a couple performance improvments and cleaning up some unused code

--- Program 2/main.rkt
+++ Program 2/main.rkt
@@ -1,44 +1,39 @@
 #lang racket
 
 (define (string->wordlist string)
-  (regexp-split #rx"[^A-Za-zÀ-ž][^A-Za-zÀ-ž']*" string))
+  (regexp-match* #rx"[A-Za-zÀ-ž][A-Za-zÀ-ž']*" string))
 
-(define (get-frequency-table wordlist)
-  (foldl
-   (λ (word table) (hash-update table word add1 0))
-   (hash)
-   (map string-downcase wordlist)))
+(define (get-frequencies words)
+  (for/fold ([count 0] [table (hash)])
+            ([word (in-list words)])
+    (values (add1 count) (hash-update table (string-downcase word) add1 0))))
 
-(define (get-subhash full-hash keys)
-  (for/hash ([k (in-list keys)])
-    (values k (hash-ref full-hash k))))
+(struct profile (name word-count frequencies))
 
-(define (get-relative-frequency-table frequency-table)
-  (let ([total-words (for/sum ([count (in-hash-values frequency-table)]) count)])
-    (for/hash ([(word count) (in-hash frequency-table)]) (values word (/ count total-words)))))
-
-(define (get-author-profile author-name)
+(define (get-profile author-name)
   (call-with-input-file (format "~a.txt" author-name)
     (λ (in-port)
-      (list author-name
-            (get-relative-frequency-table
-             (get-frequency-table (string->wordlist (port->string in-port))))))))
-
-(define (frequency-diff a-freq b-freq) (abs (log (/ a-freq b-freq) 10)))
+      (let-values ([(word-count frequencies) (get-frequencies (string->wordlist (port->string in-port)))])
+        (profile author-name word-count frequencies)))))
 
 (define (difference profile-a profile-b)
-  (let*-values ([(common-keys) (filter (curry hash-has-key? (second profile-b)) (hash-keys (second profile-a)))]
-                [(total-diff count) (for/fold ([sum 0] [count 0])
-                                              ([word (in-list common-keys)])
-                                      (values (+ sum (frequency-diff (hash-ref (second profile-a) word)
-                                                                     (hash-ref (second profile-b) word)))
-                                              (add1 count)))])
-    (/ total-diff count)))  ; no disjoint sets, pls
+  (define (frequency-diff a-freq b-freq) (abs (log (/ a-freq b-freq) 10)))
+  (let*-values ([(total-diff common-word-count)
+                 (for/fold ([sum 0] [common-word-count 0])
+                           ([word (in-hash-keys (profile-frequencies profile-a))]
+                            #:when (hash-has-key? (profile-frequencies profile-b) word))
+                   (values (+ sum (frequency-diff
+                                   (/ (hash-ref (profile-frequencies profile-a) word)
+                                      (profile-word-count profile-a))
+                                   (/ (hash-ref (profile-frequencies profile-b) word)
+                                      (profile-word-count profile-b))))
+                           (add1 common-word-count)))])
+    (/ total-diff common-word-count)))  ; no disjoint sets, pls
 
 (define (get-closest-match mystery authors)
   (argmin (curry difference mystery) authors))
 
-(let ([labelled (list (get-author-profile "Lovecraft") (get-author-profile "Doyle"))]
-      [unlabelled (list (get-author-profile "mystery1") (get-author-profile "mystery2"))])
+(let ([labelled (list (get-profile "Lovecraft") (get-profile "Doyle"))]
+      [unlabelled (list (get-profile "mystery1") (get-profile "mystery2"))])
   (for ([mystery (in-list unlabelled)])
-    (printf "'~a' was most likely written by '~a'.~n" (first mystery) (first (get-closest-match mystery labelled)))))
+    (printf "'~a' was most likely written by '~a'.~n" (profile-name mystery) (profile-name (get-closest-match mystery labelled)))))
