572d40e19c164909799f02d8734499956bad2754
creallfluharty <creallfluharty@gmail.com>
2023-04-10 00:58:54 -0500

    just do the thing

--- Program 3/main.rkt
+++ Program 3/main.rkt
@@ -48,23 +48,21 @@
   (read-line (current-input-port)))
 
 (define (command-handler command stack)
-  (do [f <- (case command
-              [("ADD") (success (curry stack-op (compose success +)))]
-              [("SUB") (success (curry stack-op (compose success -)))]
-              [("MUL") (success (curry stack-op (compose success *)))]
-              [("DIV") (success (curry stack-op safe-div))]
-              [("CLR") (success stack-clear)]
-              [("SHOW") (success (curry stack-display-result stack-show))]
-              [("TOP") (success (curry stack-display-result stack-top))]
-              [("SIZ") (success (curry stack-display-result (compose success length)))]
-              [("DUP") (success stack-duplicate-top)]
-              [("END") (failure "normal exit")] ; TODO handle this better
-              [else (let ([num (string->number command)])
-                      (if num
-                          (success (curry stack-push num))
-                          (failure (format "Unrecognized command '~a'!" command))))])]
-    [new-stack <- (f stack)]
-    (success new-stack)))
+  (case command
+    [("ADD") (stack-op (compose success +) stack)]
+    [("SUB") (stack-op (compose success -) stack)]
+    [("MUL") (stack-op (compose success *) stack)]
+    [("DIV") (stack-op safe-div stack)]
+    [("CLR") (stack-clear stack)]
+    [("SHOW") (stack-display-result stack-show stack)]
+    [("TOP") (stack-display-result stack-top stack)]
+    [("SIZ") (stack-display-result (compose success length) stack)]
+    [("DUP") (stack-duplicate-top stack)]
+    [("END") (failure "normal exit")] ; TODO handle this better
+    [else (let ([num (string->number command)])
+            (if num
+                (stack-push num stack)
+                (failure (format "Unrecognized command '~a'!" command))))]))
 
 (define (commands-handler commands stack)
   (if (empty? commands)
